c19c247b6ae6842d54043162ca5c1f029851c232
Vikky-V <vivekraj.offcial@gmail.com>
2026-01-19 21:35:39 +0530

    Implements multivariable linear regression

--- linear-reg/linear_regression.py
+++ linear-reg/linear_regression.py
@@ -1,39 +1,43 @@
+import numpy as np
 class LinearRegression:
-    def __init__(self, m = 0, b = 0, lr = 0.0001, epochs = 1000):
-        self.m = m
-        self.b = b
-        self.lr = lr
+    def __init__(self, weights = None, bias = 0, alpha = 0.0001, epochs = 1000):
+        self.weights = weights
+        self.bias = bias
+        self.alpha = alpha
         self.epochs = epochs
 
-    def _gradient_descent(self, x:list, y:list):
-        features_length = len(x)
-        dm = 0
-        db = 0
-
-        for i in range(features_length):
-            
-            y_predicted = self.m * x[i] + self.b
-            # print(f"{x[i]}  {y[i]}  {y_predicted}")
-            error = y[i] - y_predicted
-            dm += ((2 * x[i] * error) * -1)
-            db += ((2 * error) * -1)
-
-        self.m -= (self.lr * (dm / features_length))
-        self.b -= (self.lr * (db / features_length))
-        return self.m, self.b
+    def _gradient_descent(self, X, Y):
+        data_length = len(X)
+        if self.weights == None:
+            self.weights = np.zeros(len(X[0]))
+        weights_gradients = np.zeros(len(X[0]))
+        bias_gradient = 0
         
+        for i in range(data_length):
+            features = np.array(X[i])
+            prediction = np.dot(self.weights, features) + self.bias
+            error = prediction - Y[i]
+            weights_gradients += features * error
+            bias_gradient += error
+        
+        self.weights = self.weights - self.alpha * (weights_gradients / data_length)
+        self.bias = self.bias - self.alpha * bias_gradient / data_length
 
-    
-    def fit(self, X:list, Y:list):
+        return self.weights, self.bias
+
+
+    def fit(self, X, Y):
         for _ in range(self.epochs):
-            self.m, self.b = self._gradient_descent(X, Y)
+            self.weights, self.bias = self._gradient_descent(X, Y)
+
 
     def predict(self, X):
         test_length = len(X)
         test_prediction = []
         for i in range(test_length):
-            y = self.m * X[i] + self.b
-            test_prediction.append(y)
+            features = np.array(X[i])
+            y = np.dot(self.weights, features) + self.bias
+            test_prediction.append(round(float(y), 3))
         return test_prediction
     
 
--- linear-reg/test.py
+++ linear-reg/test.py
@@ -2,20 +2,19 @@ import pandas as pd
 import numpy as np
 from linear_regression import LinearRegression
 
-age_height_df = pd.read_csv('./age_height.csv')
+age_height_df = pd.read_csv('age_height.csv')
 X = age_height_df['Age']
 Y = age_height_df['Height(cm)']
+
 # age_height_df.info()
-
-X = X.to_list()
-Y = Y.to_list()
-
-print(X)
+X = np.reshape(X,(-1, 1))
+# print(X)
 model = LinearRegression()
 
 model.fit(X, Y)
 
 test = [30, 40, 21, 34]
+test = np.reshape(test, (-1, 1))
 
 prediction = model.predict(test)
 print(prediction)
